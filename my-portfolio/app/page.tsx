2147624b2865b46756dbc98d473307e548bda487
Pritesh2k <priteshparekh237@gmail.com>
2025-09-17 13:50:07 +0100

    Finsihed the Projects section

---
+++ my-portfolio/app/page.tsx
@@ -0,0 +1,25 @@
+import Hero from "@/Components/Hero";
+import Grid from "@/Components/Grid";
+import { FloatingNav } from "@/Components/ui/FloatingNav";
+import { FaHome, FaPen, FaUser } from "react-icons/fa";
+
+export default function Home() {
+  return (
+    <main className="relative flex flex-col min-h-screen h-screen bg-black-100 text-white overflow-x-hidden">
+      {/* Floating navigation at the top */}
+      <FloatingNav
+        navItems={[
+          { name: "Home", link: "/", icon: <FaHome /> },
+          { name: "Projects", link: "/projects", icon: <FaPen /> },
+          { name: "Contact", link: "/contact", icon: <FaUser /> },
+        ]}
+      />
+
+      {/* Content container */}
+      <div className="w-full max-w-7xl mx-auto flex flex-col justify-center items-center px-5 sm:px-10 mt-10">
+        <Hero />
+        <Grid />
+      </div>
+    </main>
+  );
+}
