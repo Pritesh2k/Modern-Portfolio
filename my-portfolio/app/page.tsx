420cbda4775851f6ad6c59542f3ad5ef7d98f517
Pritesh2k <priteshparekh237@gmail.com>
2025-09-18 12:45:55 +0100

    Additional touches

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,18 +1,24 @@
+// Home.tsx
 import Hero from "@/Components/Hero";
 import Grid from "@/Components/Grid";
 import { FloatingNav } from "@/Components/ui/FloatingNav";
-import { FaHome, FaPen, FaUser } from "react-icons/fa";
-import {RecentProjects} from "@/Components/RecentProjects";
+import { RecentProjects } from "@/Components/RecentProjects";
 import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative flex flex-col min-h-screen bg-black-100 text-white overflow-x-hidden">
       {/* Floating navigation at the top */}
-      <FloatingNav navItems={navItems}
-      />
-      <div className="w-full max-w-7xl mx-auto flex flex-col justify-center items-center px-5 sm:px-10 mt-10">
-        <Hero />
+      <FloatingNav navItems={navItems} />
+
+      {/* Hero fixed in background */}
+      <Hero />
+
+      {/* Spacer to push content below the hero initially */}
+      <div className="h-screen w-full" />
+
+      {/* Scrollable content */}
+      <div className="relative z-20 w-full max-w-7xl mx-auto flex flex-col justify-center items-center px-5 sm:px-10">
         <Grid />
         <RecentProjects />
       </div>
---
+++ my-portfolio/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f5f917988da6fa684b074a2c5cde71ac@o4510040248090624.ingest.de.sentry.io/4510040250253392",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
