e42e3c0212fbe5737ad4576f6e99f7a7b8322663
Pritesh2k <priteshparekh237@gmail.com>
2025-10-22 20:30:39 +0100

    Page Setup

---
+++ app/Pages/Hero.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Hero = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-red-600'>
+      Hero
+    </div>
+  );
+}
